b0ea88102e09e3de3e59ff7a5d7e08a17b28ceb1
Yoga Prastyo <yogaprastyobayu@gmail.com>
2024-12-24 19:54:10 +0700

    add: break and continue

---
+++ 12_BreakAndContinue/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    /**
+     * Break
+     * Digunakan untuk keluar dari perulangan lebih awal, meskipun kondisi
+     * perulangan belum false.
+     */
+    cout << endl << "Break" << endl;
+    for (int i = 0; i <= 10; i++) {
+        if (i == 7) {
+            break;
+        }
+
+        cout << "Angka: " << i << endl;
+    }
+
+    /**
+     * Continue
+     * Digunakan untuk melewati sisa iterasi saat ini dan melanjutkan ke iterasi
+     * berikutnya.
+     */
+    cout << endl << "Continue" << endl;
+    for (int i = 0; i <= 10; i++) {
+        if (i == 7) {
+            continue;
+            ;
+        }
+
+        cout << "Angka: " << i << endl;
+    }
+
+    return 0;
+}
