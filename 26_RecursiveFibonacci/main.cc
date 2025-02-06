61582408e86109458801c672dab487bb9459ebe3
Yoga Prastyo <yogaprastyobayu@gmail.com>
2025-02-06 17:51:47 +0700

    add: Recursive Fibonacci

---
+++ 26_RecursiveFibonacci/main.cc
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+long fibonacci(long n);
+
+int main(){
+	
+	long angka,hasil;
+	cout << "menghitung fibonacci ke : ";
+	cin >> angka;
+	hasil = fibonacci(angka);
+
+	cout << "nilainya adalah : ";
+	cout << hasil << endl;
+
+	return 0;
+}
+
+long fibonacci(long n){
+	cout << "fibonacci ke " << n << endl;
+	if((n == 0)||(n == 1)){
+		return n;
+	}else{
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
